65eaefe147af5f3d93a4b6a912403652505790f3
codefeng <863838174@qq.com>
2017-06-05 22:03:25 +0800

    波哥把四个页面的数据和图片弄好了

--- src/pages/category/category.vue
+++ src/pages/category/category.vue
@@ -1,10 +1,28 @@
 <template>
 	<div id="category">
 		<div class="category_header">
-			<form action="">	
+			<form action="">
+				<span></span>	
 				<input type="text" placeholder="请输入商品名称">
 			</form>
 		</div>
+		<span class="sousuo">搜索</span>
+		<ul>
+			<li class="category_item">
+				<div class="category_item_title">
+					<span class="category_item_icon"></span>
+					<div class="category_item_des">
+						<p>进口水果</p>
+						<p>奇异果/车厘子/牛油果/柑桔橙柚</p>
+					</div>
+					<span class="category_item_more"></span>
+				</div>
+				<div class="category_item_wrap">
+					<a href="">全部</a><a href="">奇异果</a><a href="">车厘子</a>
+					<a href="">全部</a><a href="">奇异果</a><a href="">车厘子</a>
+				</div>
+			</li>
+		</ul>
 	</div>
 </template>
 
@@ -24,17 +42,55 @@ export default{
 	/*margin-left: 15px; */
 }
 .category_header form{
-	width: 75.31%;
+	width: 2.41rem;
 }
+
+.category_header form span{
+	display: block;
+	overflow: hidden;
+    position: absolute;
+    top: 12px;
+    left: 30px;
+    width: 21px;
+    height: 21px;
+    text-indent: -999px;
+	background: url(../../../static/img/category/icon_search.png) no-repeat;
+	background-size: 100% 100%;
+}
+
 .category_header form input {
 	height: 35px;
-	width: 75.31%;
+	width: 100%;
 	background: #f8f8f8;
 	font-size: 14px;
 	color: #333;
 	border: none;
+	border-radius: 25px;
 	outline: none;
 	padding: 0 20px 0 50px;
 }
-
+.sousuo{
+	display: block;
+    width: 43px;
+    height: 45px;
+    line-height: 45px;
+    padding-right: 15px;
+    color: #008842;
+    font-size: 14px;
+    position: absolute;
+    top: 0;
+    right: 0;
+}
+.category_item_title{
+	width: 100%;
+	height: 74px;
+	padding: 15px;
+}
+.category_item_icon{
+	display: inline-block;
+	width: 48px;
+	height: 48px;
+	background: url(../../../static/img/category/icon-catalog.png) no-repeat;
+	background-position: 288px 96px;
+}
 </style>
