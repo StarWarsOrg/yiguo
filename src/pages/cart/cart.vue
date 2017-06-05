0d56b98bda27291e2e13001efae3e3f3d7d677ef
longen0707 <810727188@qq.com>
2017-06-05 22:24:38 +0800

    ‘购物车页面头部

--- src/pages/cart/cart.vue
+++ src/pages/cart/cart.vue
@@ -1,6 +1,11 @@
 <template>
-	<div id="cart">
-		<h1>cart</h1>
+	<div class="cart">
+		<div class="cart_head">
+			<p>全场满100元包邮，还差<span>100.00</span>元包邮</p>
+		</div>
+		<div class="cart_zhuru">
+			
+		</div>
 	</div>
 </template>
 
@@ -10,8 +15,21 @@ export default{
 }
 </script>
 <style>
-#cart h1 {
-	font-size: .16rem;
-	color: pink;
-}
+		.cart{
+			background:#f4f4f4;
+			height:500px;
+		}
+		.cart_head{
+			display: flex;
+			width:100%;
+			height:34px;
+			line-height:34px;
+			background:#fff9eb;
+			font-size:.14rem;
+			padding-left:9.6px;
+			margin-bottom:9.6px;
+		}
+		.cart_head p span{
+				color:red;
+		}
 </style>
